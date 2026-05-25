8ff306299a528eed33ed0a5f6a5372825a2b871d
SirioRedGiant <Oiris1992@gmail.com>
2026-05-25 17:14:52 +0200

    start to refactor and build pages

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DC Comics</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    @include('partials/header')
+    <div class="jumbotron"></div>
+    <main>
+        @yield('content')
+    </main>
+    @include('partials/footer')
+</body>
+
+</html>
